fb9cd60f1efebfc93028dca37f143910bc1d1262
Bryce Anderson <bryce.anderson22@gmail.com>
2015-07-02 02:10:00 +0000

    A better main method.

--- main.cpp
+++ main.cpp
@@ -3,30 +3,39 @@
 #include <iostream>
 #include <stdlib.h>
 
+#include <signal.h>
+
 using namespace std;
 
+bool stopI2C = false;
+
+void handleSigInt(int param) {
+  cout << "CTRL-C\n";
+  stopI2C = true;
+}
+
 int main() {
-  cout << "Hello world!\n";
+  signal(SIGINT, handleSigInt);
+
+  cout << "Attempting to measure from ADX345.\n";
 
   try {
     ADXL345 sensor(1);
     sensor.activate();
 
-    sleep(1);
+    while (!stopI2C) {
+      usleep(10*1000);
+      cout << sensor.readData().toString() << endl;
+    }
 
-    cout << sensor.readData().toString() << endl;
-
+    cout << "Shutting down." << endl;
     sensor.standby();
-
-
   }
   catch (const string& str) {
     cerr << "Failed: " << str << endl;
   }
-  catch (const char* err) {
-    cerr << "Failed: " << err << endl;
-  }
 
+  // all done.
   return 0;
 }
 
